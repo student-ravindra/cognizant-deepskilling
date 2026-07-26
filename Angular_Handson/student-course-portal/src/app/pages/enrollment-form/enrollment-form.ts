8cd1ec70ea27180c6737f14bc10243b41a34fab4
student-ravindra <ravindraeslavath2004@gmail.com>
2026-07-26 11:10:17 +0530

    Completed Angular Handson 4 : Template Driven Forms and Validation

---
+++ Angular_Handson/student-course-portal/src/app/pages/enrollment-form/enrollment-form.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-enrollment-form',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './enrollment-form.html',
+  styleUrl: './enrollment-form.css'
+})
+export class EnrollmentForm {
+
+  studentName = '';
+  studentEmail = '';
+  courseId: number | null = null;
+  preferredSemester = 'Odd';
+  agreeToTerms = false;
+
+  submitted = false;
+
+  onSubmit(form: NgForm): void {
+
+    if (form.valid) {
+      console.log(form.value);
+      console.log(form.valid);
+
+      this.submitted = true;
+    }
+  }
+
+}
